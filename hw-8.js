8bdcb37d7de4e0d56135db2117d59a8030534d5b
Крис <kris@example.com>
2026-06-12 12:21:17 +0300

    hw-8

--- hw-8.js
+++ hw-8.js
@@ -130,3 +130,92 @@ function playRockPaperScissors() {
         alert("Вы проиграли. Попробуйте снова!");
     }
 }
+
+// Задание 1
+
+const peopleForSort = [
+    { name: "Глеб", age: 29 },
+    { name: "Анна", age: 17 },
+    { name: "Олег", age: 7 },
+    { name: "Оксана", age: 47 }
+];
+
+console.log(peopleForSort.sort((a, b) => a.age - b.age));
+
+
+// Задание 2
+
+function isPositive(number) {
+    return number > 0;
+}
+
+function isMale(person) {
+    return person.gender === "male";
+}
+
+function filter(array, ruleFunction) {
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+        if (ruleFunction(array[i])) {
+            result.push(array[i]);
+        }
+    }
+
+    return result;
+}
+
+console.log(filter([3, -4, 1, 9], isPositive));
+
+const people = [
+    { name: "Глеб", gender: "male" },
+    { name: "Анна", gender: "female" },
+    { name: "Олег", gender: "male" },
+    { name: "Оксана", gender: "female" }
+];
+
+console.log(filter(people, isMale));
+
+
+// Задание 3
+
+const intervalId = setInterval(() => {
+    console.log(new Date());
+}, 3000);
+
+setTimeout(() => {
+    clearInterval(intervalId);
+    console.log("30 секунд прошло");
+}, 30000);
+
+
+// Задание 4
+
+function delayForSecond(callback) {
+    setTimeout(callback, 1000);
+}
+
+delayForSecond(function () {
+    console.log("Привет, Глеб!");
+});
+
+
+// Задание 5
+
+function delayForSecondTask5(cb) {
+    setTimeout(() => {
+        console.log("Прошла одна секунда");
+
+        if (cb) {
+            cb();
+        }
+    }, 1000);
+}
+
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+delayForSecondTask5(() => {
+    sayHi("Глеб");
+});
